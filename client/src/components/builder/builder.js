9d6ff006518aceeffe345f8557cdc5b5caf07d79
Maria Volpe <mariaCHVolpe@gmail.com>
2018-10-28 20:23:47 -0400

    Add component that asks for the job posting user is applying for

--- client/src/components/builder/builder.js
+++ client/src/components/builder/builder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import QuestionsContainer from './questions-container.js';
 import StartView from './start-view.js';
 import ResultsView from './results-view.js';
+import JobPostingInput from './job-posting-input.js';
 
 class Builder extends Component {
   constructor() {
@@ -11,13 +12,15 @@ class Builder extends Component {
     this.state = {
       isStartOpen: true,
       isOptionsOpen: false,
+      isJobPostingOpen: false,      
       isQuestionnaireOpen: false,
       isResultsOpen: false,
     };
 
     this.scroll = this.scroll.bind(this);
     this.startToOptions = this.startToOptions.bind(this);
-    this.optionsToQuestionnaire = this.optionsToQuestionnaire.bind(this);
+    this.optionsToJobPosting = this.optionsToJobPosting.bind(this);
+    this.jobPostingToQuestionnaire = this.jobPostingToQuestionnaire.bind(this);
     this.questionnaireToResults = this.questionnaireToResults.bind(this);
   }
 
@@ -25,9 +28,14 @@ class Builder extends Component {
     this.setState(() => ({isStartOpen: false, isOptionsOpen: true }));
   }
 
-  optionsToQuestionnaire() {
+  optionsToJobPosting() {
     this.elementsList = [];
-    this.setState(() => ({isOptionsOpen: false, isQuestionnaireOpen: true }));
+    this.setState(() => ({isOptionsOpen: false, isJobPostingOpen: true }));
+  }
+
+  jobPostingToQuestionnaire() {
+    this.elementsList = [];
+    this.setState(() => ({isJobPostingOpen: false, isQuestionnaireOpen: true }));
   }
 
   questionnaireToResults() {
@@ -38,8 +46,7 @@ class Builder extends Component {
     if (index < this.elementsList.length - 1) {
       this.elementsList[index + 1].scrollIntoView({ behavior: 'smooth' });
     } else if (this.state.isOptionsOpen) {
-      this.optionsToQuestionnaire();
-      this.topDiv.scrollIntoView();      
+      this.optionsToJobPosting();   
     } else {
       this.questionnaireToResults();
     }
@@ -59,6 +66,10 @@ class Builder extends Component {
               isQuestionnaire={false}
               scroll={this.scroll}
             />
+          : null
+        }
+        {this.state.isJobPostingOpen
+          ? <JobPostingInput end={this.jobPostingToQuestionnaire}/>
           : null
         }
         {this.state.isQuestionnaireOpen
