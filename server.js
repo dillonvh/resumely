e4fa3dbbad41e3f1418e85b143b63c5e9398332e
Ashia Nagi <ashianagi00@gmail.com>
2018-11-24 18:14:13 -0500

    create server.js and controller folder

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ app.get("*", function(req,res){
 	res.send("Error");
 }); 
 
-app.listen(3000, function(){
+app.listen(5000, function(){
 	console.log("server is listening");
 });
-
-//I wasn't able to view this when I ran the server. I don't know how to. Can you show me? 
-//Let me know if I did anything wrong. 
