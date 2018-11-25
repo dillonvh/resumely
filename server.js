75a67e3867d9025fb8efc867fa828948817717dc
Maria Volpe <mariaCHVolpe@gmail.com>
2018-11-25 18:25:35 -0500

    Add minor styling fixes

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var express = require("express");
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get("/", function(req,res) {
-	console.log("This is working")
-	res.send("Hello World");
+app.get('/', function (req,res) {
+	console.log('This is working')
+	res.send('Hello World');
 });
 
-app.get("*", function(req,res){
-	res.send("Error");
-}); 
+app.get('*', function (req,res) {
+	res.send('404 Error');
+});
 
-app.listen(5000, function(){
-	console.log("server is listening");
+app.listen(5000, function () {
+	console.log('Server is listening on port 5000');
 });
